474d4b79390253c8617ca0d10c83384f48ac40f1
abufire2025 <abufire2025@gmail.com>
2026-03-07 00:39:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 
-api = '8229350603:AAG42YW2p5ypjaFkVwAHwSUsHBAfe7ZHexI'
-bot = Bot(token=api)
+API_TOKEN = "8229350603:AAG42YW2p5ypjaFkVwAHwSUsHBAfe7ZHexI"
+
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 @dp.message()
 async def handle_messages(message: types.Message):
-if not message.text:
-return
+    if not message.text:
+        return
+    
+    await message.answer("Xabar qabul qilindi!")
 
 async def main():
-await dp.start_polling(bot)
+    await dp.start_polling(bot)
 
-if name == "main":
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
